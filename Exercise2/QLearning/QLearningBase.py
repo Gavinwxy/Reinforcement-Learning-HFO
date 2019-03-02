dd58f034065f927c3e08f11b6b223eef6aec15f5
gavinwxy <xiaoyang.wong318@gmail.com>
2019-03-02 21:44:59 +0000

    Implement Q learning

--- Exercise2/QLearning/QLearningBase.py
+++ Exercise2/QLearning/QLearningBase.py
@@ -3,26 +3,79 @@
 
 from DiscreteHFO.HFOAttackingPlayer import HFOAttackingPlayer
 from DiscreteHFO.Agent import Agent
+import argparse
+import numpy as np
 
 class QLearningAgent(Agent):
 	def __init__(self, learningRate, discountFactor, epsilon, initVals=0.0):
 		super(QLearningAgent, self).__init__()
+		self.learningRate = learningRate
+		self.discountFactor = discountFactor
+		self.inivals = initVals
+		self.qTable = {} # Contains all state-action pairs: {(S):{(S,A):value,...},...}
+		self.epsilon = epsilon
+		self.episodeRecord = []
 		
 
 	def learn(self):
-		raise NotImplementedError
+		nextState = self.episodeRecord[-1][0]
+		# Check if next sate is in q table
+		if not nextState in self.qTable:
+			self.qTable[nextState] = self.stateInit(nextState)
+		
+		currentState = self.episodeRecord[-2][0]
+		action = self.episodeRecord[-2][1]
+		reward = self.episodeRecord[-2][2]
+		currentValue = self.qTable[currentState][(currentState, action)]
+
+		# Obtain target value according to target policy
+		targetValue = max([value for value in self.qTable[nextState].values()])
+
+		error = self.learningRate*(reward+self.discountFactor*targetValue-currentValue)
+		self.qTable[currentState][(currentState, action)] += error
+
+		return targetValue - currentValue
 
 	def act(self):
-		raise NotImplementedError
+		# Epsilon greedy
+		values = []
+		actions = []
+		# Select actions according to probs
+		allPossibleActions = self.qTable[self.state]
+		for action, actionValue in allPossibleActions.items():
+			actions.append(action[1])
+			values.append(actionValue)
+		# Looking for all possible optimal actions
+		optAct = [actions[i] for i, x in enumerate(values) if x == max(values)]
+		# Setting epsilon greedy algorithm
+		probs = [1-self.epsilon, self.epsilon]
+		indicator = np.random.choice([0,1], p=probs)
+		# Choose optimal action with tie breaking
+		if indicator == 0: 
+			action = np.random.choice(optAct)
+		# Randomly choose one action
+		else:
+			action = np.random.choice(actions)
+
+		return action
+
+	def stateInit(self, state):
+		qInit = {}
+		for action in self.possibleActions:
+			qInit[(state, action)] = 0
+		
+		return qInit
 
 	def toStateRepresentation(self, state):
-		raise NotImplementedError
+		return state[0]
 
 	def setState(self, state):
-		raise NotImplementedError
+		if not state in self.qTable:
+			self.qTable[state] = self.stateInit(state)
+		self.state = state
 
 	def setExperience(self, state, action, reward, status, nextState):
-		raise NotImplementedError
+		self.episodeRecord = [(state, action, reward), nextState]
 
 	def setLearningRate(self, learningRate):
 		raise NotImplementedError
@@ -37,6 +90,15 @@ class QLearningAgent(Agent):
 		raise NotImplementedError
 
 if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--id', type=int, default=0)
+	parser.add_argument('--numOpponents', type=int, default=0)
+	parser.add_argument('--numTeammates', type=int, default=0)
+	parser.add_argument('--numEpisodes', type=int, default=500)
+
+	args=parser.parse_args()
+
 	# Initialize connection with the HFO server
 	hfoEnv = HFOAttackingPlayer(numOpponents = args.numOpponents, numTeammates = args.numTeammates, agentId = args.id)
 	hfoEnv.connectToServer()
@@ -52,7 +114,7 @@ if __name__ == '__main__':
 		observation = hfoEnv.reset()
 		
 		while status==0:
-			learningRate, epsilon = agent.computeHyperparameters(self, numTakenActions, episode)
+			learningRate, epsilon = agent.computeHyperparameters(numTakenActions, episode)
 			agent.setEpsilon(epsilon)
 			agent.setLearningRate(learningRate)
 			
