b553a14f3e420d64411ba3c1b6bcb2af6e2a6158
gavinwxy <xiaoyang.wong318@gmail.com>
2019-03-02 14:54:44 +0000

    Implementation of SARSA

--- Exercise2/SARSA/SARSABase.py
+++ Exercise2/SARSA/SARSABase.py
@@ -3,38 +3,102 @@
 
 from DiscreteHFO.HFOAttackingPlayer import HFOAttackingPlayer
 from DiscreteHFO.Agent import Agent
+import argparse
+import numpy as np 
 
 class SARSAAgent(Agent):
-	def __init__(self, learningRate, discountFactor, epsilon, initVals=0.0):
+	def __init__(self, learningRate, discountFactor, epsilon=0.1, initVals=0.0):
 		super(SARSAAgent, self).__init__()
+		self.learningRate = learningRate
+		self.discountFactor = discountFactor
+		self.inivals = initVals
+		self.qTable = {} # Contains all state-action pairs: {(S):{(S,A):value,...},...}
+		self.epsilon = epsilon
+		self.episodeRecord = []
 
 	def learn(self):
-		raise NotImplementedError
+		# The current state
+		state1 = self.episodeRecord[-2][0]
+		action1 = self.episodeRecord[-2][1]
+		reward1 = self.episodeRecord[-2][2]
+		value1 = self.qTable[state1][(state1,action1)]
+
+		# The next state
+		state2 = self.episodeRecord[-1][0]
+		action2 = self.episodeRecord[-1][1]
+		value2 = self.qTable[state2][(state2, action2)]
+
+		# Update q table
+		self.qTable[state1][(state1,action1)] += reward1 + self.discountFactor*value2 - value1
 
 	def act(self):
-		raise NotImplementedError
+		values = []
+		actions = []
+		# Select actions according to probs
+		allPossibleActions = self.qTable[self.state]
+		for action, actionValue in allPossibleActions.items():
+			actions.append(action[1])
+			values.append(actionValue)
+		# Looking for all possible optimal actions
+		optAct = [actions[i] for i, x in enumerate(values) if x == max(values)]
+		# Setting epsilon greedy algorithm
+		probs = [1-self.epsilon, self.epsilon]
+		indicator = np.random.choice([0,1], p=probs)
+		# Choose optimal action with tie breaking
+		if indicator == 0: 
+			action = np.random.choice(optAct)
+		# Randomly choose one action
+		else:
+			action = np.random.choice(actions)
+
+		return action
+
+	def stateInit(self, state):
+		qInit = {}
+		for action in self.possibleActions:
+			qInit[(state, action)] = 0
+		
+		return qInit
+
 
 	def setState(self, state):
-		raise NotImplementedError
+		if not state in self.qTable:
+			self.qTable[state] = self.stateInit(state)
+
+		self.state = state # This state should be (1,1) form
 
 	def setExperience(self, state, action, reward, status, nextState):
-		raise NotImplementedError
+		if action == None:
+			action = np.random.choice(self.possibleActions)
+		self.episodeRecord.append((state, action, reward))
+
+
+	def computeHyperparameters(self, numTakenActions, episodeNumber):
+		return self.learningRate, self.epsilon
 
 	def toStateRepresentation(self, state):
-		raise NotImplementedError
+		return state[0]
 
 	def reset(self):
-		raise NotImplementedError
+		self.episodeReturn = []
 
 	def setLearningRate(self, learningRate):
-		raise NotImplementedError
+		self.learningRate = learningRate
 
 	def setEpsilon(self, epsilon):
-		raise NotImplementedError
+		self.epsilon = epsilon
 
 if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--id', type=int, default=0)
+	parser.add_argument('--numOpponents', type=int, default=0)
+	parser.add_argument('--numTeammates', type=int, default=0)
+	parser.add_argument('--numEpisodes', type=int, default=500)
+
+	args=parser.parse_args()
 	
-	numEpisode = 10
+	numEpisodes = args.numEpisodes
 	# Initialize connection to the HFO environment using HFOAttackingPlayer
 	hfoEnv = HFOAttackingPlayer(numOpponents = args.numOpponents, numTeammates = args.numTeammates, agentId = args.id)
 	hfoEnv.connectToServer()
@@ -53,7 +117,10 @@ if __name__ == '__main__':
 		epsStart = True
 
 		while status==0:
-			learningRate, epsilon = agent.computeHyperparameters(self, numTakenActions, episode)
+			learningRate, epsilon = agent.computeHyperparameters(numTakenActions, episode)
+			agent.setEpsilon(epsilon)
+			agent.setLearningRate(learningRate)
+			
 			obsCopy = observation.copy()
 			agent.setState(agent.toStateRepresentation(obsCopy))
 			action = agent.act()
@@ -72,5 +139,3 @@ if __name__ == '__main__':
 
 		agent.setExperience(agent.toStateRepresentation(nextObservation), None, None, None, None)
 		agent.learn()
-
-	
